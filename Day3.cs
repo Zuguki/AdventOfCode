865fb7c89a72949e1f5ac3a0f7f61adfeba34701
Zuguki <guskov.zahar20@gmail.com>
2021-12-15 14:23:30 +0300

    Task1 done

--- Day3.cs
+++ Day3.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
 namespace AdventOfCode
 {
     public class Day3
@@ -6,5 +12,33 @@ namespace AdventOfCode
         private const string FileTwoPath = "../../../Files/Day3SecondTask.txt";
         
         private const string FileTestPath = "../../../Files/Test.txt";
+
+        public static int Task1()
+        {
+            var lines = GetFileStrings(FileOnePath);
+            var oneValues = new int[lines[0].Length];
+
+            foreach (var line in lines)
+            {
+                for (var index = 0; index < line.Length; index++)
+                {
+                    oneValues[index] += line[index] == '1' ? 1 : -1;
+                }
+            }
+
+            var sBinaryMain = new StringBuilder();
+            var sBinaryAddition = new StringBuilder();
+            foreach (var value in oneValues)
+            {
+                sBinaryMain.Append(value > 0 ? 1 : 0);
+                sBinaryAddition.Append(value < 0 ? 1 : 0);
+            }
+            
+            return Convert.ToInt32(sBinaryMain.ToString(), 2)
+                   * Convert.ToInt32(sBinaryAddition.ToString(), 2);
+        }
+
+        private static List<string> GetFileStrings(string filePath = FileTestPath) => File.ReadAllLines(filePath)
+            .ToList();
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace AdventOfCode
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(Day2.Task1());
-            Console.WriteLine(Day2.Task2());
+            Console.WriteLine(Day3.Task1());
+            // Console.WriteLine(Day2.Task2());
         }
     }
 }
