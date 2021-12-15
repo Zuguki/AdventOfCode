9e541c9f08edb6996dd2c81a12536381192493e1
Zuguki <guskov.zahar20@gmail.com>
2021-12-15 14:07:09 +0300

    Day3 init

---
+++ Day3.cs
@@ -0,0 +1,10 @@
+namespace AdventOfCode
+{
+    public class Day3
+    {
+        private const string FileOnePath = "../../../Files/Day3FirstTask.txt";
+        private const string FileTwoPath = "../../../Files/Day3SecondTask.txt";
+        
+        private const string FileTestPath = "../../../Files/Test.txt";
+    }
+}
