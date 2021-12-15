a175f380b2b17c7b6e19f34127ea9dd7c1c3a4cb
Zuguki <guskov.zahar20@gmail.com>
2021-12-15 21:26:55 +0300

    Task1 init

---
+++ Day5.cs
@@ -0,0 +1,13 @@
+namespace AdventOfCode
+{
+    public static class Day5
+    {
+        private const string FileOnePath = "../../../Files/Day4FirstTask.txt";
+        private const string FileTestPath = "../../../Files/Test.txt";
+
+        public static int Task1()
+        {
+            return 1;
+        }
+    }
+}
