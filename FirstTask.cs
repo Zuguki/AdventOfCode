67ce03044b9b2e1361e5dcfa49f6c68199bcd5ae
Zuguki <guskov.zahar20@gmail.com>
2021-12-15 11:51:26 +0300

    Task1 done

--- FirstTask.cs
+++ FirstTask.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 namespace AdventOfCode
 {
     public static class FirstTask
     {
-        private static string _filePath = "Files/Day1FirstTask.txt";
+        private const string FilePath = "../../../Files/Day1FirstTask.txt";
+
+        public static int Task1()
+        {
+            var numbers = GetFileInts();
+            return numbers.Skip(1)
+                .Where((number, i) => number > numbers[i])
+                .Count();
+        }
+
+        private static List<int> GetFileInts() => File.ReadAllLines(FilePath)
+            .Select(int.Parse)
+            .ToList();
     }
 }
