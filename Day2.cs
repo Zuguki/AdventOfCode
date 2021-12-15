23ae679be10766b48b641977dc86a11e5b92a6cc
Zuguki <guskov.zahar20@gmail.com>
2021-12-15 12:04:27 +0300

    Day2 init

---
+++ Day2.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class Day2
+    {
+        private const string FileOnePath = "../../../Files/Day2FirstTask.txt";
+        private const string FileTwoPath = "../../../Files/Day2SecondTask.txt";
+    }
+}
