f7ff9857874a441175c553b58e9b04e5491cb3d8
Zuguki <guskov.zahar20@gmail.com>
2021-12-16 20:04:23 +0300

    Task1 done

--- Day8.cs
+++ Day8.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 namespace AdventOfCode
 {
     public static class Day8
@@ -5,9 +10,43 @@ namespace AdventOfCode
         private const string FileOnePath = "../../../Files/Day8FirstTask.txt";
         private const string FileTestPath = "../../../Files/Test.txt";
 
+        private static readonly Dictionary<int, string> Dict = new()
+        {
+            {0, "abcefg"},
+            {1, "cf"},
+            {2, "acdeg"},
+            {3, "acdfg"},
+            {4, "bcdf"},
+            {5, "abdfg"},
+            {6, "abdefg"},
+            {7, "acf"},
+            {8, "abcdefg"},
+            {9, "abcdfg"}
+        };
+
         public static int Task1()
         {
-            return 1;
+            var segments = GetFileInput(FileOnePath);
+            var correct = 0;
+            foreach (var segment in segments)
+            {
+                correct += segment
+                    .Count(segmentVal => 
+                        segmentVal.Length == Dict[1].Length 
+                        || segmentVal.Length == Dict[4].Length 
+                        || segmentVal.Length == Dict[7].Length 
+                        || segmentVal.Length == Dict[8].Length);
+            }
+            
+            return correct;
         }
+
+        private static List<string[]> GetFileInput(string fileName = FileTestPath) => File.ReadAllLines(fileName)
+                .Select(s => s.Split('|'))
+                .Select(segmentArr => segmentArr[1]
+                    .Trim()
+                    .Split(' ')
+                    .ToArray())
+                .ToList();
     }
 }
