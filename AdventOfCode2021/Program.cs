734bee46b8e9b897afdfba18da42e105df8e1046
Zuguki <guskov.zahar20@gmail.com>
2023-01-30 08:11:20 +0300

    AdventOfCode with folders

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Day8.Task1());
+            Console.WriteLine(Day8.Task2());
+        }
+    }
+}
